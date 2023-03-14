b6074d24ffe629144ad27a02ca0f9c816bbde963
Fabio Souza <fabiohrsouza@gmail.com>
2023-03-13 23:50:56 -0300

    Aplicação do card

---
+++ card-apresentacao/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+
+  const usuario = { 
+    nome: "Fábio H R Souza", 
+    profissao: "Full Stack Developer - Jr", 
+    foto: "https://avatars.githubusercontent.com/u/92132323?s=400&u=ee2fe1703475d8bbaf9fab62cc3e826465e79090&v=4", 
+    github: "https://github.com/FabioHRSouza", 
+    linkedin: "https://www.linkedin.com/in/fabiohrsouza" }
+
+  return (
+    <div>
+      <Card usuario={usuario} />
+    </div>
+  )
+}
+
+export default App
